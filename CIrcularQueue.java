f2ac5d88dead65308ca1b3980ec68acb2512670d
Nikhil Sharma <68590314+nikhil3400@users.noreply.github.com>
2020-09-21 22:28:23 +0530

    Add files via upload

---
+++ CIrcularQueue.java
@@ -0,0 +1,56 @@
+class circularq {
+	int a[] = new int[5];
+	int front=-1,rear=-1,f=0;
+
+	void enqueue(int n){
+	 if(front<0) {
+	  front++;
+	  rear++;
+	  a[front]=n;
+	  }
+	 else {
+	   if(rear == 4) {
+	   	 f=1;
+	     System.out.print("overflow");
+	   }
+	   else {
+	   	 rear++;
+	   	 a[rear]=n;
+	   }
+	 }
+	}
+
+	void dequeue() {
+	  if(front==-1 && rear==-1) {
+	  System.out.print("Underflow");
+	  }
+	  else if(front==rear) {
+	    front = rear = -1;
+	  }
+	  else {
+	    front++;
+	  }
+	}
+
+	void display() {
+		if(f==0) {
+		for(int i=front;i<=rear;i++)
+			System.out.print(a[i]+" ");
+	     }
+	}
+	
+}
+
+class CircularQueue {
+	public static void main(String args[]) {
+	  circularq ob = new circularq();
+	    ob.enqueue(1);
+		ob.enqueue(2);
+		ob.enqueue(3);
+		ob.enqueue(4);
+		ob.enqueue(5);
+		ob.dequeue();
+		ob.enqueue(6);
+		ob.display();
+	}
+}
